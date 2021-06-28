7a47ae563188c8ae96ba4ccc34fa636586a3102f
tuanhophh <trantuanhop9678@gmail.com>
2021-06-28 16:49:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello <code>src/App.js</code> and save to reload.
+          alo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
